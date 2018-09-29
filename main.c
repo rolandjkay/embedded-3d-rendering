73d5f29cba689cda8f1904f1884d0196ef9e65e3
Roland Kay <roland.j.kay@googlemail.com>
2018-09-29 20:17:22 +0100

    * Adjust field of view * Make rostra of ships * Rotate on X and Z axis.

--- main.c
+++ main.c
@@ -14,19 +14,30 @@
 static Camera camera = CAMERA_INIT;
 
 static const Vector look_point = VECTOR_INIT(0.0, 0.0, 0.0);
-static float near_plane = -20.0;
+static float near_plane = -50.0;
 static float near_plane_width = 60.0;
 static float far_plane = -160.0;
 static SimpleRenderer renderer;
+static Scene scene = SCENE_INIT;
+
+static const Object* ships[] = {
+  &viper, &cobra, &missile
+};
 
 void update(Display* display, uint32_t clock)
 {
   // Move the camera
-  camera_set_location(&camera, 50 * sin(clock / 1000.), 50 * cos(clock / 1000.), 130.0);
+  float clock_ = 0.0;
+  camera_set_location(&camera, 50 * sin(clock_ / 1000.), 50 * cos(clock_ / 1000.), 230.0);
   camera_set_up_vector(&camera, 0.0, 1.0, 0.0);
 
   //camera_set_location(&camera, 0., 200.0, 0.0);
   //camera_set_up_vector(&camera, 0.0, 0.0, -1.0);
+
+  matrix_tf_rotation(&scene.scene_objects[0].rotation_matrix,
+                     2.*PI*clock/8000.0, 0.0, 2.*PI*clock/8000.0);
+
+  scene.scene_objects[0].object = ships[(clock / 10000) % countof(ships)];
 
   camera_set_look_point(&camera, 0.0, 0.0, 0.0);
   camera_calc_transforms(&camera);
@@ -44,17 +55,14 @@ int main( int argc, char* args[] )
       return 0;
   }
 
-  Scene scene = SCENE_INIT;
   scene.scene_objects[0].object = &viper;
-  vector_init(&scene.scene_objects[0].location, 0.0, 40.0, 0.0, 1.0);
+  vector_init(&scene.scene_objects[0].location, 0.0, 0.0, 0.0, 1.0);
   matrix_identity(&scene.scene_objects[0].rotation_matrix);
 
-  scene.scene_objects[1].object = &cobra;
-  vector_init(&scene.scene_objects[1].location, 0.0, -20.0, 0.0, 1.0);
-  matrix_tf_rotation(&scene.scene_objects[1].rotation_matrix,
-                     0.0, PI, 0.0);
-
-  printf("%s\n", matrix_to_str(&scene.scene_objects[1].rotation_matrix));
+  //scene.scene_objects[1].object = &cobra;
+  //vector_init(&scene.scene_objects[1].location, 0.0, -20.0, 0.0, 1.0);
+  //matrix_tf_rotation(&scene.scene_objects[1].rotation_matrix,
+  //                   0.0, PI, 0.0);
 
   sr_init(&renderer, &scene, near_plane, far_plane, near_plane_width);
 
