a4b721ad68afbb2b2541abc476f50bde2b9247b4
Roland Kay <roland.j.kay@googlemail.com>
2018-10-05 21:00:55 +0100

    Update
    * tidy up magic numbers
    * update pixel format conversion

--- sdl/display_impl_sdl.c
+++ sdl/display_impl_sdl.c
@@ -1,10 +1,8 @@
 #include "display_impl_sdl.h"
 #include "../defs.h"
 #include "../errors.h"
-#include "../utils.h"
 #include <sys/time.h>
 #include <SDL2/SDL.h>
-
 
 
 int display_impl_init(DisplayImpl* self)
@@ -19,12 +17,12 @@ int display_impl_init(DisplayImpl* self)
   self->_renderer = NULL;
   self->_screen = NULL;
   self->_texture = NULL;
-  self->_width = 128;
-  self->_height = 64;
   memset(self->_pixels, 0, sizeof(self->_pixels));
   memset(self->_buffer, 0, sizeof(self->_buffer));
 
-  SDL_CreateWindowAndRenderer(self->_width, self->_height, SDL_WINDOW_SHOWN,
+  SDL_CreateWindowAndRenderer(SDL_SCREEN_WIDTH,
+                              SDL_SCREEN_HEIGHT,
+                              SDL_WINDOW_SHOWN,
                               &self->_window, &self->_renderer);
 
   if (!self->_window || !self->_renderer)
@@ -42,7 +40,8 @@ int display_impl_init(DisplayImpl* self)
   self->_texture = SDL_CreateTexture(self->_renderer,
                                      SDL_PIXELFORMAT_RGB332,
                                      SDL_TEXTUREACCESS_STATIC,
-                                     self->_width, self->_height);
+                                     SDL_SCREEN_WIDTH,
+                                     SDL_SCREEN_HEIGHT);
   if (!self->_texture)
   {
     set_error(ERROR_DISPLAY_HW_ERROR);
@@ -66,16 +65,16 @@ int display_impl_init(DisplayImpl* self)
 void display_impl_cls(DisplayImpl* self)
 {
   // Blank display
-  memset(self->_pixels, 0, self->_width * self->_height);
+  memset(self->_pixels, 0, SDL_SCREEN_BUFFER_SIZE);
 
-  SDL_UpdateTexture(self->_texture, NULL, self->_pixels, self->_width);
+  SDL_UpdateTexture(self->_texture, NULL, self->_pixels, SDL_SCREEN_WIDTH);
 }
 
 void display_impl_show(DisplayImpl* self)
 {
   SDL_UpdateTexture(self->_texture, NULL,
                     self->_pixels,
-                    self->_width);
+                    SDL_SCREEN_WIDTH);
   SDL_RenderClear(self->_renderer);
   SDL_RenderCopy(self->_renderer, self->_texture, NULL, NULL);
   SDL_RenderPresent(self->_renderer);
@@ -89,9 +88,9 @@ static uint8_t display_pixel_colour(DisplayImpl* display, int16_t x, int16_t y)
 
 void display_impl_draw_pixel(DisplayImpl* display, int16_t x, int16_t y)
 {
-  if (x < display->_width && x >= 0 && y < display->_height && y >= 0)
+  if (x < SDL_SCREEN_WIDTH && x >= 0 && y < SDL_SCREEN_HEIGHT && y >= 0)
   {
-    *(display->_pixels + y * display->_width + x) = display_pixel_colour(display, x, y);
+    *(display->_pixels + y * SDL_SCREEN_WIDTH + x) = display_pixel_colour(display, x, y);
   }
 }
 
@@ -146,25 +145,64 @@ void display_impl_draw_col_pixel(DisplayImpl* display, int16_t x, int16_t y)
 }*/
 
 /*
+ * Translate 1 byte pp horizontal layout to 1 bit pp vertical layout of
+ * virtual screen buffer.
+ */
+static void horiz_1byte_to_vert_1bit(const uint8_t input[SDL_SCREEN_BUFFER_SIZE],
+                                     uint8_t output[SDL_VIRTUAL_SCREEN_BUFFER_SIZE])
+{
+  // Block is 8 lines across the screen.
+  static const size_t BLOCK_SIZE = SDL_SCREEN_WIDTH*8;
+
+  // The SSD1306 screen is made up of eight horizontal blocks each of which
+  // is 8 pixels high.
+  uint8_t* out_ptr = &output[0];
+  for (size_t block_start = 0 ;
+       block_start < SDL_SCREEN_BUFFER_SIZE;
+       block_start += BLOCK_SIZE)
+  {
+    *out_ptr = 0;
+    for (size_t column_index = 0; column_index < SDL_SCREEN_WIDTH; ++column_index, ++out_ptr)
+    {
+      // Highest byte (on the screen) is LSB
+      for (size_t pixel_index = 0; pixel_index < 8; ++pixel_index)
+      {
+        uint8_t pixel_value = input[block_start + pixel_index * SDL_SCREEN_WIDTH + column_index];
+        *out_ptr |= pixel_value ? (1<<pixel_index) : 0;
+      }
+    }
+  }
+}
+
+/*
+ * Inverse of above
+ */
+static void vert_1bit_to_horiz_1byte(const uint8_t input[1024],
+                                     uint8_t output[1024*8])
+{
+  // NB: Not x8 this time, as 1 bit pp.
+  static const size_t BLOCK_SIZE = SDL_SCREEN_WIDTH;
+  uint8_t* out_ptr = &output[0];
+
+  for (size_t block_start = 0 ; block_start < 1024; block_start += BLOCK_SIZE)
+  {
+    // Select LSB (1) first, then 2, then 4...
+    for (uint8_t mask=1; mask != 0; mask <<= 1)
+    {
+      for (size_t column_index = 0; column_index < BLOCK_SIZE; ++column_index, ++out_ptr)
+      {
+        *out_ptr = (input[block_start + column_index] & mask) ? 0xff : 0;
+      }
+    }
+  }
+}
+
+/*
  *  Translate 1 byte pp layout to 1 bit pp layout of virtual screen buffer.
  */
 uint8_t* display_impl_get_buffer(DisplayImpl* display)
 {
-  uint8_t tmp[1024];
-  uint8_t* pixels_ptr = display->_pixels;
-  for (int i = 0; i < countof(display->_buffer); ++i, pixels_ptr += 8)
-  {
-    tmp[i] = (*(pixels_ptr + 0) ? 0b10000000 : 0 )
-           | (*(pixels_ptr + 1) ? 0b01000000 : 0 )
-           | (*(pixels_ptr + 2) ? 0b00100000 : 0 )
-           | (*(pixels_ptr + 3) ? 0b00010000 : 0 )
-           | (*(pixels_ptr + 4) ? 0b00001000 : 0 )
-           | (*(pixels_ptr + 5) ? 0b00000100 : 0 )
-           | (*(pixels_ptr + 6) ? 0b00000010 : 0 )
-           | (*(pixels_ptr + 7) ? 0b00000001 : 0 );
-  }
-
-  util_horiz_pixel_buf_to_vert(tmp, display->_buffer);
+  horiz_1byte_to_vert_1bit(display->_pixels, display->_buffer);
 
   return display->_buffer;
 }
@@ -174,17 +212,5 @@ uint8_t* display_impl_get_buffer(DisplayImpl* display)
  */
 void display_impl_release_buffer(DisplayImpl* display)
 {
-  uint8_t* pixels_ptr = display->_pixels;
-  uint8_t tmp[1024];
-
-  util_vert_pixel_buf_to_horiz(display->_buffer, tmp);
-
-  for (int i = 0; i < countof(tmp); ++i)
-  {
-    uint8_t byte = tmp[i];
-    for (int j = 0; j < 8; ++j, ++pixels_ptr)
-    {
-      *pixels_ptr = (byte & (1 << (7-j)) ? 0xff : 0x00);
-    }
-  }
+  vert_1bit_to_horiz_1byte(display->_buffer, display->_pixels);
 }
--- sdl/display_impl_sdl.h
+++ sdl/display_impl_sdl.h
@@ -3,26 +3,31 @@
 
 #include <SDL2/SDL.h>
 
+// Size in bytes of the backing buffer for the SDL display (1 byte per pixel)
+// and the "virtual" buffer, whih simulates the layout of the SSD1306 chip.
+static const size_t SDL_SCREEN_WIDTH = 128;
+static const size_t SDL_SCREEN_HEIGHT = 64;
+static const size_t SDL_SCREEN_BUFFER_SIZE = SDL_SCREEN_WIDTH*SDL_SCREEN_HEIGHT;
+static const size_t SDL_VIRTUAL_SCREEN_BUFFER_SIZE = SDL_SCREEN_WIDTH*SDL_SCREEN_HEIGHT/8;
+
 typedef struct
 {
   SDL_Window* _window;
   SDL_Renderer* _renderer;
   SDL_Surface* _screen;
   SDL_Texture * _texture;
-  uint8_t _pixels[1024];
-  int _width;
-  int _height;
+  uint8_t _pixels[SDL_SCREEN_BUFFER_SIZE];
   // Virtual screen buffer in 1 bit per pixel layout.
   // - SDL doesn't support this, so we have to marshal back and forth between
   //   its 1 byte per pixel format.
-  uint8_t _buffer[1024];
+  uint8_t _buffer[SDL_VIRTUAL_SCREEN_BUFFER_SIZE];
 } DisplayImpl;
 
 #define DISPLAY_IMPL_INIT {0}
 
 int display_impl_init(DisplayImpl* self);
-static inline int display_impl_get_width(DisplayImpl* self) { return self->_width; }
-static inline int display_impl_get_height(DisplayImpl* self) { return self->_height; }
+static inline int display_impl_get_width(DisplayImpl* self) { return SDL_SCREEN_WIDTH; }
+static inline int display_impl_get_height(DisplayImpl* self) { return SDL_SCREEN_HEIGHT; }
 void display_impl_cls(DisplayImpl* self);
 void display_impl_show(DisplayImpl* self);
 void display_impl_draw_pixel(DisplayImpl* self, int16_t x, int16_t y);
