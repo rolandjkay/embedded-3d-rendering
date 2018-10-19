436a59f51b1417f106b00aea1fd12f4ce11f82e2
Roland Kay <roland.j.kay@googlemail.com>
2018-10-19 21:27:40 +0100

    Optimization
    Use powers of 2 to avoid library division

--- animation/typed_string.c
+++ animation/typed_string.c
@@ -10,30 +10,27 @@
 
 void typed_string_init(TypedString* self,
                        pgm_ptr_t str,
-                       size_t ms_per_char,
+                       size_t log_ms_per_char,
                        size_t column,
                        size_t row)
 {
   self->_str = str;
   self->_strlen = strlen_P((char*)str);
-  self->_ms_per_char = ms_per_char;
+  self->_log_ms_per_char = log_ms_per_char;
   self->_column = column;
   self->_row = row;
 }
 
 void typed_string_render(TypedString* self, Display* display, uint32_t clock)
 {
+  size_t n = clock >> self->_log_ms_per_char;
+
   // Display clock / ms-per-char characters.
   // XXX Easiest to assume that string is in progmem atm.
-  font_write_string_at_text_pos_P(display,
-                                  self->_str,
-                                  clock / self->_ms_per_char,
-                                  0,0);
+  font_write_string_at_text_pos_P(display, self->_str, n, 0,0);
 
-  if (clock % 1000 < 500)
+  if ((clock & 1024) < 512)
   {
-    font_write_char_at_text_pos(display, '_',
-                                MIN(self->_strlen, clock / self->_ms_per_char),
-                                0);
+    font_write_char_at_text_pos(display, '_', MIN(self->_strlen, n), 0);
   }
 }
--- animation/typed_string.h
+++ animation/typed_string.h
@@ -12,14 +12,14 @@ typedef struct
 {
   pgm_ptr_t _str;
   size_t _strlen;
-  size_t _ms_per_char;
+  size_t _log_ms_per_char;
   size_t _column;
   size_t _row;
 } TypedString;
 
 void typed_string_init(TypedString* self,
                        pgm_ptr_t str,
-                       size_t ms_per_char,
+                       size_t log_ms_per_char,
                        size_t column,
                        size_t row);
 void typed_string_render(TypedString* self, Display* display, uint32_t clock);
